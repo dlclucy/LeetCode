8bf56109c9e5d4af048a3c2d3a83e7ca038af852
yangguang <yangguang@yangguangdeMacBook-Pro.local>
2017-10-31 21:26:43 +0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+class HouseRobber {
+public:
+    int rob(vector<int> &num){
+        if(num.size()<=1)
+            return num.empty()?0:num[0];
+
+    }
+
+    int main() {
+        int p;
+        cin >> p;
+        std::cout << "Hello, World!" << std::endl;
+        return 0;
+    }
+};
